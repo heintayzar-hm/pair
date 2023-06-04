2f5fcb7b2b933af8aebb0d32e152abdce76ff929
Hein Tay Zar <heintayzarhm@gmail.com>
2023-06-04 20:00:04 +0630

    User experience selection page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if resource.experience_level.nil?
+      user_experience_path
+    else
+      home_index_path
+    end
+  end
 end
---
+++ app/controllers/user_selections_controller.rb
@@ -0,0 +1,20 @@
+class UserSelectionsController < ApplicationController
+  def experience
+    # Render the experience selection view
+  end
+
+  def create_experience
+    # Process and save the experience selection
+    # Redirect to the programming languages selection page
+  end
+
+  def programming_languages
+    # Render the programming languages selection view
+  end
+
+  def create_programming_languages
+    # Process and save the programming languages selection
+    # Redirect to the desired page (e.g., user dashboard)
+  end
+
+end
